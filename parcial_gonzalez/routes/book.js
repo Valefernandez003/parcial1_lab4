18bcc89563a4eb6021000a721c4f68c2a3a3a5aa
Valefernandez003 <141738312+Valefernandez003@users.noreply.github.com>
2025-05-05 12:10:07 -0300

    Update book.js

--- parcial_gonzalez/routes/book.js
+++ parcial_gonzalez/routes/book.js
@@ -6,6 +6,7 @@ router.get('/:id',ctrl.getById);
 router.post('/',ctrl.create);
 router.put('/:id',ctrl.update);
 router.delete('/:id',ctrl.delete);
+router.get('/:id',ctrl.validation);
 
 module.exports = router;
 
